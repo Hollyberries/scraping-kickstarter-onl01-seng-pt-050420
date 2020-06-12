e00dda072155b93580e95560db423159db167ed5
Holly Miranda-Brennecke <loyaltothesport85@gmail.com>
2020-06-12 21:58:17 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -16,4 +16,3 @@ end
 # location: project.css("ul.project-meta span.location-name").text
 # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 
-def create_project_hash
